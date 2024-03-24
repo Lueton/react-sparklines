7c6b5c6520900e36b3812ce507a7f67179a2981f
Lueton <leon.luettger@web.de>
2024-03-24 11:06:28 +0100

    feat: added displayName for Tooltip component

--- lib/components/Tooltip/Tooltip.tsx
+++ lib/components/Tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
-import isFunction from "lodash/isFunction"
+import isFunction from "lodash/isFunction";
 import {
   cloneElement,
   createElement,
@@ -6,45 +6,47 @@ import {
   isValidElement,
   ReactElement,
   ReactNode,
-} from "react"
+} from "react";
 
-import { DataKey } from "../../utils/types.ts"
-import { DefaultTooltipContent } from "./DefaultTooltipContent.tsx"
-import { TooltipWrapper } from "./TooltipWrapper.tsx"
+import { DataKey } from "../../utils/types.ts";
+import { DefaultTooltipContent } from "./DefaultTooltipContent.tsx";
+import { TooltipWrapper } from "./TooltipWrapper.tsx";
 
-export type TooltipContent = ReactElement | ((props: TooltipProps) => ReactNode) | undefined
+export type TooltipContent = ReactElement | ((props: TooltipProps) => ReactNode) | undefined;
 
 export interface TooltipPayload {
-  name?: DataKey
-  value?: number
-  dataKey?: DataKey
-  color?: string
+  name?: DataKey;
+  value?: number;
+  dataKey?: DataKey;
+  color?: string;
 }
 
 export interface TooltipProps {
-  content?: TooltipContent
-  contentStyle?: CSSProperties
-  coords?: { x: number; y: number }
-  itemStyle?: CSSProperties
-  label?: string | number
-  labelStyle?: CSSProperties
-  payload?: Array<TooltipPayload>
-  separator?: string
-  wrapperStyle?: CSSProperties
+  content?: TooltipContent;
+  contentStyle?: CSSProperties;
+  coords?: { x: number; y: number };
+  itemStyle?: CSSProperties;
+  label?: string | number;
+  labelStyle?: CSSProperties;
+  payload?: Array<TooltipPayload>;
+  separator?: string;
+  wrapperStyle?: CSSProperties;
 }
 
 const renderContent = (content: TooltipContent, props: TooltipProps) => {
-  if (isValidElement(content)) return cloneElement(content, props)
-  if (isFunction(content)) return createElement(content, props)
-  return <DefaultTooltipContent {...props} />
-}
+  if (isValidElement(content)) return cloneElement(content, props);
+  if (isFunction(content)) return createElement(content, props);
+  return <DefaultTooltipContent {...props} />;
+};
 
 export const Tooltip = (props: TooltipProps) => {
-  const { content, coords = { x: 0, y: 0 }, wrapperStyle = {} } = props
+  const { content, coords = { x: 0, y: 0 }, wrapperStyle = {} } = props;
 
   return (
     <TooltipWrapper coords={coords} style={wrapperStyle}>
       {renderContent(content, props)}
     </TooltipWrapper>
-  )
-}
+  );
+};
+
+Tooltip.displayName = "Tooltip";
