630603a3f0a3974d1f5e7ee09a2311d05a81452f
Lueton <leon.luettger@web.de>
2024-06-09 14:50:45 +0200

    feat: formatting BREAKING CHANGE: try to force major release

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,15 @@
 export { Bar, Line, ReferenceLine } from "./cartesian";
 export { Tooltip } from "./components";
 export { SparklinesBar, SparklinesComposed, SparklinesLine } from "./sparklines";
-export type {BarProps, LineProps, ReferenceLineProps, SparklinesBarProps, SparklinesComposedProps, SparklinesDataEntry, SparklinesEventData, SparklinesLineProps, TooltipPayload,TooltipProps} from "./utils/types";
+export type {
+  BarProps,
+  LineProps,
+  ReferenceLineProps,
+  SparklinesBarProps,
+  SparklinesComposedProps,
+  SparklinesDataEntry,
+  SparklinesEventData,
+  SparklinesLineProps,
+  TooltipPayload,
+  TooltipProps,
+} from "./utils/types";
