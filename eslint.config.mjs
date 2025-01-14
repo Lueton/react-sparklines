502374fb42ee8d051be8ac20a7585908b394d430
Lueton <leon.luettger@web.de>
2025-01-14 16:20:51 +0100

    chore: added new final eslint config to git

---
+++ eslint.config.mjs
@@ -0,0 +1,95 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { fixupPluginRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "prettier",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        react,
+        "react-hooks": fixupPluginRules(reactHooks),
+        prettier,
+        "simple-import-sort": simpleImportSort,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                tsx: true,
+            },
+        },
+    },
+
+    settings: {
+        "import/resolver": {
+            typescript: {},
+        },
+
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {
+        camelcase: "error",
+        "no-duplicate-imports": "error",
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "react/react-in-jsx-scope": "off",
+        "no-console": "error",
+        "no-alert": "error",
+        "react-hooks/exhaustive-deps": "off",
+        "react/prop-types": 0,
+        "react/display-name": 0,
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+        "@typescript-eslint/no-empty-function": "off",
+        "react/no-unknown-property": "off",
+        "react/no-unescaped-entities ": "off",
+        "@typescript-eslint/no-unused-expressions": "off",
+        "@typescript-eslint/no-unused-vars": ["error", {
+            ignoreRestSiblings: true,
+        }],
+    },
+}, {
+    files: ["**/.eslintrc.{js,cjs,json}"],
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        ecmaVersion: 5,
+        sourceType: "commonjs",
+    },
+}];
